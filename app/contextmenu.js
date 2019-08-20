14c0694c37c79aa1cfe8e41d21d483c034c8746e
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-08-20 18:53:50 +0800

    Add files via upload

--- app/contextmenu.js
+++ app/contextmenu.js
@@ -1,38 +1,57 @@
-const {app, BrowserWindow,Menu,MenuItem, Tray,shell,clipboard,dialog,Notification} = require('electron');
-function contextMenu(event,params) {
+const { Menu, MenuItem,  clipboard,  Notification,shell} = require('electron');
+
+function contextMenu(event, params) {
     const menu = new Menu();
-    if(params.isEditable) {
-        menu.append(new MenuItem({label:'undo', role: 'undo' }));
-        menu.append(new MenuItem({label:'redo', role: 'redo' }));
-        // menu.append(new MenuItem({ role: 'separator' }));
-        menu.append(new MenuItem({label:'cut', role: 'cut' }));
-        menu.append(new MenuItem({label:'paste', role: 'paste' }));
+    if (params.isEditable) {
+        menu.append(new MenuItem({label: 'undo', role: 'undo'}));
+        menu.append(new MenuItem({label: 'redo', role: 'redo'}));
+        menu.append(new MenuItem({label: 'cut', role: 'cut'}));
+        menu.append(new MenuItem({label: 'paste', role: 'paste'}));
         // menu.append(new MenuItem({ role: 'pasteandmatchstyle' }));
-        menu.append(new MenuItem({label:'delete', role: 'delete' }));
+        menu.append(new MenuItem({label: 'delete', role: 'delete'}));
     }
-    if(params.linkURL){
+    if (params.linkURL) {
         let linkURL = params.linkURL;
         let linkText = params.linkText;
         menu.append(new MenuItem({
+           label:"open the link at External Browser",
+           click:function () {
+               shell.openExternal(linkURL).then();
+           }
+        }));
+        menu.append(new MenuItem({
                 label: "copy link address",
-                click: function (_,focusWindow) {
+                click: function () {
                     clipboard.writeText(linkURL);
                 }
             }
         ));
         menu.append(new MenuItem({
-            label:"copy link Text",
-                click:function () {
-            clipboard.writeText(linkText);
-            // new Notification({
-            //     title: "copy",
-            //     body: "copy TEXT: " + linkText
-            // }).show();
-        }
+            label: "copy link Text",
+            click: function () {
+                clipboard.writeText(linkText);
+            }
         }))
     }
 
-    if(params.selectionText) {
+    menu.append(new MenuItem({
+        label: 'go forward',
+        accelerator: 'Alt+Right',
+        click: function (_, focusedWindow) {
+            if (focusedWindow.webContents.canGoForward()) focusedWindow.webContents.goForward()
+        }
+    }));
+
+
+    menu.append(new MenuItem({
+        label: 'go back',
+        accelerator: 'Alt+Left',
+        click: function (_, focusedWindow) {
+            if (focusedWindow.webContents.canGoBack()) focusedWindow.webContents.goBack()
+        }
+    }));
+
+    if (params.selectionText) {
         menu.append(new MenuItem({
             label: 'copy',
             // role: 'copy',
@@ -46,23 +65,23 @@ function contextMenu(event,params) {
         }));
     }
     menu.append(new MenuItem({
-        label:'copy this page URL',
-        click:function (_,focusWindow) {
+        label: 'copy this page URL',
+        click: function (_, focusWindow) {
             clipboard.writeText(focusWindow.webContents.getURL());
-            // new Notification({
-            //     title: "copy",
-            //     body: "copy TEXT: " + focusWindow.webContents.getURL()
-            // }).show();
         }
     }));
-    menu.append(new MenuItem({ label:'select all', role: 'selectall' }));
+    menu.append(new MenuItem({label: 'select all', role: 'selectall'}));
     menu.append(new MenuItem({
-        label:'refresh',
-        click:function (_,focusWindow) {
+        label: 'refresh',
+        click: function (_, focusWindow) {
             focusWindow.reload()
         }
     }));
-    menu.popup(this, params.x, params.y);
+    menu.popup({
+        window:this,
+        x:params.x,
+        y:params.y
+    });
 }
 
-exports.contextMenu=contextMenu;
+exports.contextMenu = contextMenu;
