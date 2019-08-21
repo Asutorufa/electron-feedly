bab81529bbe4fa68f21fdf2ce3ff70dc87fb4155
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2019-08-21 17:01:16 +0800

    Add files via upload

--- app/contextmenu.js
+++ app/contextmenu.js
@@ -1,4 +1,4 @@
-const { Menu, MenuItem,  clipboard,  Notification,shell} = require('electron');
+const { Menu, MenuItem,  BrowserWindow,clipboard,  Notification,shell,nativeImage} = require('electron');
 
 function contextMenu(event, params) {
     const menu = new Menu();
@@ -10,6 +10,64 @@ function contextMenu(event, params) {
         // menu.append(new MenuItem({ role: 'pasteandmatchstyle' }));
         menu.append(new MenuItem({label: 'delete', role: 'delete'}));
     }
+
+    if (params.srcURL) {
+        if (params.hasImageContents) {
+            menu.append(new MenuItem({
+                label: "view image",
+                click: () => {
+                    new BrowserWindow({
+                        width: 500,
+                        height: 480,
+                        webPreferences: {
+                            preload: require('path').join(__dirname, 'preload.js'),
+                            // nodeIntegration: true,
+                            nativeWindowOpen: true
+                        },
+                        title: 'view media'
+                    }).loadURL(params.srcURL).then();
+                }
+            }));
+            menu.append(new MenuItem({
+                label: 'copy image',
+                click: (_, focusWindow) => {
+                    focusWindow.webContents.copyImageAt(params.x, params.y);
+                    // const request = require('request').defaults({encoding: null});
+                    // request.get(params.srcURL, function (error, response, body) {
+                    //     // https://stackoverflow.com/questions/17124053/node-js-get-image-from-web-and-encode-with-base64
+                    //     let imageData;
+                    //     if (!error && response.statusCode === 200) {
+                    //         imageData = "data:" + response.headers["content-type"] +
+                    //             ";base64," + new Buffer(body).toString('base64');
+                    //         clipboard.writeImage(nativeImage.createFromDataURL(imageData));
+                    //     }
+                    // });
+                }
+            }));
+            menu.append(new MenuItem({
+                label: "copy image link",
+                click: () => {
+                    console.log(params.srcURL);
+                    clipboard.writeText(params.srcURL);
+                }
+            }));
+        }else{
+            menu.append(new MenuItem({
+                label: "copy media link",
+                click: () => {
+                    console.log(params.srcURL);
+                    clipboard.writeText(params.srcURL);
+                }
+            }));
+            menu.append(new MenuItem({
+                label: "download media",
+                click: (_, focusWindow) => {
+                    focusWindow.loadURL(params.srcURL);
+                }
+            }));
+        }
+    }
+
     if (params.linkURL) {
         let linkURL = params.linkURL;
         let linkText = params.linkText;
@@ -34,36 +92,48 @@ function contextMenu(event, params) {
         }))
     }
 
-    menu.append(new MenuItem({
-        label: 'go forward',
-        accelerator: 'Alt+Right',
-        click: function (_, focusedWindow) {
-            if (focusedWindow.webContents.canGoForward()) focusedWindow.webContents.goForward()
-        }
-    }));
-
-
-    menu.append(new MenuItem({
-        label: 'go back',
-        accelerator: 'Alt+Left',
-        click: function (_, focusedWindow) {
-            if (focusedWindow.webContents.canGoBack()) focusedWindow.webContents.goBack()
-        }
-    }));
-
     if (params.selectionText) {
         menu.append(new MenuItem({
             label: 'copy',
             role: 'copy'
-            // click: function (_, focusWindow) {
-            //     focusWindow.webContents.copy();
-            //     // new Notification({
-            //     //     title: "copy",
-            //     //     body: "copy TEXT: " + params.selectionText
-            //     // }).show();
-            // }
+        }));
+        menu.append(new MenuItem({
+            label: 'search from google',
+            click:()=>{
+                new BrowserWindow({
+                    // transparent: true,
+                    // frame: false,
+                    width: 1200,
+                    height: 700,
+                    webPreferences: {
+                        preload: require('path').join(__dirname, 'preload.js'),
+                        // nodeIntegration: true,
+                        nativeWindowOpen: true
+                    }
+                }).loadURL("https://www.google.com/search?q=" + params.selectionText).then();
+            }
+        }))
+    }
+
+    if(BrowserWindow.getFocusedWindow().webContents.canGoForward()){
+        menu.append(new MenuItem({
+            label: 'go forward',
+            accelerator: 'Alt+Right',
+            click: function (_, focusedWindow) {
+                focusedWindow.webContents.goForward()
+            }
         }));
     }
+    if(BrowserWindow.getFocusedWindow().webContents.canGoBack()) {
+        menu.append(new MenuItem({
+            label: 'go back',
+            accelerator: 'Alt+Left',
+            click: function (_, focusedWindow) {
+                focusedWindow.webContents.goBack()
+            }
+        }));
+    }
+    
     menu.append(new MenuItem({
         label: 'copy this page URL',
         click: function (_, focusWindow) {
